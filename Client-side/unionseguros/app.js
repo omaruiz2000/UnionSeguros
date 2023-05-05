5989e08eaf1c1de2acc9bb6c1af697134013be00
omaruiz2000 <89617743+omaruiz2000@users.noreply.github.com>
2023-05-05 17:44:47 -0500

    Update app.js

--- Client-side/unionseguros/app.js
+++ Client-side/unionseguros/app.js
@@ -14,7 +14,7 @@ var adminDetallePlanSOATRouter = require('./routes/adminDetallePlanSOAT');
 const app = express();
 
 // Start the server
-const PORT = process.env.PORT || 3000
+const PORT = 3000
 app.listen(PORT, function () {
   console.log(`Server listening on port ${PORT}`)
 })
