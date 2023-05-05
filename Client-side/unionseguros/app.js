6c74280ba01e47ba8465ffdb8d582cf2538bf40a
omaruiz2000 <89617743+omaruiz2000@users.noreply.github.com>
2023-05-05 13:37:44 -0500

    Update app.js

--- Client-side/unionseguros/app.js
+++ Client-side/unionseguros/app.js
@@ -11,6 +11,14 @@ var SOATRouter = require('./routes/SOAT');
 var adminPlanSOATRouter = require('./routes/adminPlanSOAT');
 var adminDetallePlanSOATRouter = require('./routes/adminDetallePlanSOAT');
 
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('A Monk in Cloud'); //write a response to the client
+  res.end(); //end the response
+}).listen(80); //the server object listens on port 80
+
 var app = express();
 
 // view engine setup
